c828c1bc3dcdb076f0bdca83894ddce0173d1a5a
rhyths08 <rhythmritvik@gmail.com>
2022-07-03 10:50:53 +0530

    added code

---
+++ TREE-BINARY TREE/Binary Tree/BinaryTreeLL.java
@@ -0,0 +1,107 @@
+import java.util.LinkedList;
+import java.util.Queue;
+public class BinaryTreeLL {
+    BinaryNode root;
+    public BinaryTreeLL(){
+        this.root = null;
+    }
+
+    //PreOrder Traversal
+    void preOrder(BinaryNode node){
+        if(node == null){
+            return;
+        }
+        System.out.print(node.value + " " );
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    //InOrder Traversal
+    void inOrder(BinaryNode node){
+        if(node == null){
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.value + " " );
+        inOrder(node.right);
+    }
+
+    //PostOrder Traversal
+    void postOrder(BinaryNode node){
+        if(node == null){
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " " );
+    }
+
+    //LevelOrder
+    void levelOrder(){
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            BinaryNode presentNode = queue.remove();
+            System.out.print(presentNode.value + " ");
+            if(presentNode.left != null){
+                queue.add(presentNode.left);
+            }
+            if(presentNode.right != null){
+                queue.add(presentNode.right);
+            }
+        }
+    }
+
+    //Search Node
+    public void search(String value){
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            BinaryNode presentNode = queue.remove();
+            if(presentNode.value == value){
+                System.out.println("The value- "+value+" is found in Tree");
+                return;
+            }
+            else{
+                if(presentNode.left != null){
+                    queue.add(presentNode.left);
+                }
+                if(presentNode.right != null){
+                    queue.add(presentNode.right);  
+                }
+            }
+        }
+        System.out.println("The value- "+value+" is not found in Tree");
+    }
+
+    //Insert Method
+    void insert(String value){
+        BinaryNode newNode = new BinaryNode();
+        newNode.value = value;
+        if(root == null){
+            root = newNode;
+            System.out.println("Inserted new node at root");
+            return;
+        }
+
+        Queue<BinaryNode> queue = new LinkedList<BinaryNode>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            BinaryNode presentNode = queue.remove();
+            if(presentNode.left == null){
+                presentNode.left = newNode;
+                System.out.println("Successfully Inserted");
+                break;
+            }
+            else if(presentNode.right == null){
+                presentNode.right = newNode;
+                System.out.println("Successfully Inserted");
+                break;
+            }
+            else{
+                queue.add(presentNode.left);
+                queue.add(presentNode.right);
+            }
+        }
+   }
+}
---
+++ TREE-BINARY TREE/Binary Tree/Main.java
@@ -0,0 +1,54 @@
+public class Main{
+    public static void main(String[] args) {
+        BinaryTreeLL binaryTree = new BinaryTreeLL();
+        /*BinaryNode N1 = new BinaryNode();
+        N1.value = "N1";
+        BinaryNode N2 = new BinaryNode();
+        N2.value = "N2";
+        BinaryNode N3 = new BinaryNode();
+        N3.value = "N3";
+        BinaryNode N4 = new BinaryNode();
+        N4.value = "N4";
+        BinaryNode N5 = new BinaryNode();
+        N5.value = "N5";
+        BinaryNode N6 = new BinaryNode();
+        N6.value = "N6";
+        BinaryNode N7 = new BinaryNode();
+        N7.value = "N7";
+        BinaryNode N8 = new BinaryNode();
+        N8.value = "N8";
+        BinaryNode N9 = new BinaryNode();
+        N9.value = "N9";
+
+        N1.left = N2;
+        N1.right = N3;
+        N2.left = N4;
+        N2.right = N5;
+        N4.left = N8;
+        N4.right = N9;
+        N3.left = N6;
+        N3.right = N7;
+        binaryTree.root = N1;
+
+        System.out.println("PreOrder Traversal: ");
+        binaryTree.preOrder(binaryTree.root);
+        System.out.println("\nInOrder Traversal: ");
+        binaryTree.inOrder(binaryTree.root);
+        System.out.println("\nPost Order Traversal: ");
+        binaryTree.postOrder(binaryTree.root);
+        System.out.println("\nLevel Order Traversal: ");
+        binaryTree.levelOrder();
+        System.out.println();
+        binaryTree.search("N5");*/
+
+        binaryTree.insert("N1");
+        binaryTree.insert("N2");
+        binaryTree.insert("N3");
+        binaryTree.insert("N4");
+        binaryTree.insert("N5");
+        binaryTree.insert("N6");
+        System.out.println("\nLevel Order Traversal: ");
+        binaryTree.levelOrder();
+        
+    }
+}
